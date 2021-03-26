9ce1a28f356c65794bf6a25caa032a005ff74349
Yassine EL MERZOUKI <yassinemerzouki.ym@gmail.com>
2021-03-26 16:59:23 +0100

    modifie template

--- core/templatetags/cart_template_tags.py
+++ core/templatetags/cart_template_tags.py
@@ -7,8 +7,10 @@ register = template.Library()
 def cart_product_count(user):
     if user.is_authenticated:
         qs = Order.objects.filter(user= user, ordered=False)
-
+        total = 0
         if qs.exists():
-            return qs[0].products.count()
+            for product in qs[0].products.all():
+                total += product.quantity
+        return total
     return 0
 
